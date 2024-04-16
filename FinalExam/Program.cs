22b4ce2ef1db16e69e31073d61a33ac84a707c97
shinymew151 <shinymilotic172@gmail.com>
2024-04-16 20:41:25 +0700

    Update Program.cs

--- FinalExam/Program.cs
+++ FinalExam/Program.cs
@@ -8,9 +8,9 @@ public class Program
         decimal amount;
 
         Console.WriteLine("Select account type (1 - Normal, 2 - Exchange):");
-#pragma warning disable CS8604 // Possible null reference argument.
+
         int choice = int.Parse(Console.ReadLine());
-#pragma warning restore CS8604 // Possible null reference argument.
+
 
         IAccountType accountType;
         if (choice == 1)
@@ -29,9 +29,9 @@ public class Program
         }
 
         Console.WriteLine("Enter initial balance:");
-#pragma warning disable CS8604 // Possible null reference argument.
+
         decimal initialBalance = decimal.Parse(Console.ReadLine());
-#pragma warning restore CS8604 // Possible null reference argument.
+
 
         account = accountType.CreateAccount(initialBalance); // Leverage interface method
 
@@ -43,9 +43,9 @@ public class Program
             Console.WriteLine("3. Exit");
 
             Console.WriteLine("Enter your choice:");
-#pragma warning disable CS8604 // Possible null reference argument.
+
             int option = int.Parse(Console.ReadLine());
-#pragma warning restore CS8604 // Possible null reference argument.
+
 
             if (option == 1)
             {
@@ -54,9 +54,9 @@ public class Program
             else if (option == 2)
             {
                 Console.WriteLine("Enter amount to transfer:");
-#pragma warning disable CS8604 // Possible null reference argument.
+
                 amount = decimal.Parse(Console.ReadLine());
-#pragma warning restore CS8604 // Possible null reference argument.
+
                 account.Transfer(amount);
             }
             else if (option == 3)
